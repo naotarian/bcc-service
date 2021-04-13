961c940f389a7650f93fc6fd3c7114754013d339
EC2 Default User <ec2-user@ip-172-31-4-35.us-east-2.compute.internal>
2021-04-13 15:21:25 +0000

    first commit

---
+++ resources/views/template/header.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('/css/style.css') }}" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div class="over_header">
+        <ul>
+            <li>サンプルテキスト</li>
+            <li>サンプルテキスト</li>
+        </ul>
+    </div>
+    <nav>
+      <ul id="main">
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills
+          <ul class="drop">
+            <div>
+            <li>scss</li>
+            <li>jquery</li>
+            <li>html</li>
+            </div>
+          </ul>
+        </li>
+        <li>Contact</li>
+        <div id="marker"></div>
+      </ul>
+    </nav>
+</body>
+</html>
