2cbec430ee70a30e732f41d0136640c61cc5d49d
EC2 Default User <ec2-user@ip-172-31-4-35.us-east-2.compute.internal>
2021-04-14 14:44:06 +0000

    slider作成

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('/css/style.css') }}" rel="stylesheet">
+    <link href="/css/slider.css" rel="stylesheet">
+    <link href="/css/slick/slick-theme.css" rel="stylesheet">
+    <link href="/css/slick/slick.css" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
-    @include('template.header');
+    @include('template.header')
+    <ul class="slider">
+      <li><img src="/images/pc/top/deli.jpeg"></li>
+      <li><img src="/images/pc/top/hell.jpeg"></li>
+      <li><img src="/images/pc/top/deli.jpeg"></li>
+      <li><img src="/images/pc/top/hell.jpeg"></li>
+    </ul>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="js/slick.js"></script>
+    <script>
+        $(document).ready(function(){
+          $('.slider').slick({
+          dots: true,
+          autoplay: true,
+          });
+        });
+    </script>
 </body>
 </html>
--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -7,6 +7,7 @@
     <title>Document</title>
 </head>
 <body>
+  <div class="pconly">
     <div class="over_header">
         <ul>
             <li>サンプルテキスト</li>
@@ -30,5 +31,156 @@
         <div id="marker"></div>
       </ul>
     </nav>
+  </div>
+  
+  <div class="sponly">
+    <div class="header_sp">
+      
+    </div>
+    <div class="el_humburger"><!--ハンバーガーボタン-->
+      <div class="el_humburger_wrapper">
+        <span class="el_humburger_bar top"></span>
+        <span class="el_humburger_bar middle"></span>
+        <span class="el_humburger_bar bottom"></span>
+      </div>
+    </div>
+   
+    <header class="navi">
+      <div class="navi_inner">
+   {{--
+        <div class="navi_item js_openParent"><a href="">WORKS</a>
+            <div class="el_spChildNavOpen js_openSwitch">
+              <div class="el_spChildNavOpen_wrapper"></div>
+            </div>
+            <div class="nav_child js_openTarget">
+              <div class="nav_child_item">
+                <a class="hp_coverLink" href=""></a>
+                WORKSの子ページ
+              </div>
+              <div class="nav_child_item">
+                <a class="hp_coverLink" href=""></a>
+                WORKSの子ページ
+              </div>
+              <div class="nav_child_item">
+                <a class="hp_coverLink" href=""></a>
+                WORKSの子ページ
+              </div>
+              <div class="nav_child_item">
+                <a class="hp_coverLink" href=""></a>
+                WORKSの子ページ
+              </div>
+              <div class="nav_child_item">
+                <a class="hp_coverLink" href=""></a>
+                WORKSの子ページ
+              </div>
+            </div>
+            <div class="bl_nav_item_spToggle"></div>
+        </div>
+        --}}
+        <div class="navi_item"><a href="">TOP</a></div>
+        <div class="navi_item"><a href="">ご利用の流れ</a></div>
+        <div class="navi_item js_openParent"><a href="">お申し込み</a>
+            <div class="el_spChildNavOpen js_openSwitch">
+              <div class="el_spChildNavOpen_wrapper"></div>
+            </div>
+            <div class="nav_child js_openTarget">
+              <div class="nav_child_item">
+                <a href=""></a>
+                scss
+              </div>
+              <div class="nav_child_item">
+                <a href=""></a>
+                jquery
+              </div>
+              <div class="nav_child_item">
+                <a href=""></a>
+                html
+              </div>
+            </div>
+            <div class="bl_nav_item_spToggle"></div>
+        </div>
+        <div class="navi_item"><a href="">よくある質問</a></div>
+        <div class="navi_item"><a href="">お問い合わせ</a></div>
+      </div>
+    </div>
+  </header>
+<!--CDNでjQuery読み込む-->
+<script
+src="https://code.jquery.com/jquery-2.2.4.min.js"
+integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+crossorigin="anonymous"></script>
 </body>
 </html>
+
+<script>
+  var navigationOpenFlag = false;
+var navButtonFlag = true;
+var focusFlag = false;
+ 
+//toggle
+$(function(){
+  $(document).on('click','.js_openSwitch',function(){
+    $(this).parent('.js_openParent').find(' > .js_openTarget').slideToggle("fast");
+    $(this).parent('.js_openParent').toggleClass('js_fire');
+  });
+});
+ 
+//ハンバーガーメニュー
+    $(function(){
+ 
+      $(document).on('click','.el_humburger',function(){
+        if(navButtonFlag){
+          spNavInOut.switch();
+          //一時的にボタンを押せなくする
+          setTimeout(function(){
+            navButtonFlag = true;
+          },200);
+          navButtonFlag = false;
+        }
+      });
+      $(document).on('click touchend', function(event) {
+        if (!$(event.target).closest('.navi,.el_humburger').length && $('body').hasClass('js_humburgerOpen') && focusFlag) {
+          focusFlag = false;
+          //scrollBlocker(false);
+          spNavInOut.switch();
+        }
+      });
+    });
+ 
+//ナビ開く処理
+function spNavIn(){
+  $('body').removeClass('js_humburgerClose');
+  $('body').addClass('js_humburgerOpen');
+  setTimeout(function(){
+    focusFlag = true;
+  },200);
+  setTimeout(function(){
+    navigationOpenFlag = true;
+  },200);
+}
+ 
+//ナビ閉じる処理
+function spNavOut(){
+  $('body').removeClass('js_humburgerOpen');
+  $('body').addClass('js_humburgerClose');
+  setTimeout(function(){
+    $(".uq_spNavi").removeClass("js_appear");
+    focusFlag = false;
+  },200);
+  navigationOpenFlag = false;
+}
+ 
+//ナビ開閉コントロール
+var spNavInOut = {
+  switch:function(){
+    if($('body.spNavFreez').length){
+      return false;
+    }
+    if($('body').hasClass('js_humburgerOpen')){
+     spNavOut();
+    } else {
+     spNavIn();
+    }
+  }
+};
+</script>
